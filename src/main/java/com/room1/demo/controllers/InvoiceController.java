33ad65122067546feb3c2dfbcd537ff0a17005e9
Justin Garner <j.garner828@gmail.com>
2022-07-13 11:03:21 -0400

    service layer and album view

--- src/main/java/com/room1/demo/controllers/InvoiceController.java
+++ src/main/java/com/room1/demo/controllers/InvoiceController.java
@@ -1,7 +1,24 @@
 package com.room1.demo.controllers;
 
+import com.room1.demo.models.Invoice;
+import com.room1.demo.service.InvoiceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class InvoiceController {
+
+    @Autowired
+    InvoiceService invoiceService;
+
+    @GetMapping("/invoice")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Invoice> getAllInvoices() {
+        invoiceService
+    }
 }
--- src/main/java/com/room1/demo/service/InvoiceService.java
+++ src/main/java/com/room1/demo/service/InvoiceService.java
@@ -1,4 +1,19 @@
 package com.room1.demo.service;
 
+import com.room1.demo.repositories.InvoiceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
+@Component
 public class InvoiceService {
+
+    private InvoiceRepository invoiceRepository;
+
+    @Autowired
+    public InvoiceService(InvoiceRepository invoiceRepository) {
+        this.invoiceRepository = invoiceRepository;
+    }
+
+
 }
