efe8187a38821083099990f42fd57128622b246f
Justin Garner <j.garner828@gmail.com>
2022-07-13 12:59:54 -0400

    updated sql schema for seed data

--- schema.sql
+++ schema.sql
@@ -1,3 +1,4 @@
+drop schema if exists game_store;
 create schema if not exists game_store;
 use game_store;
 
@@ -60,3 +61,62 @@ create table if not exists invoice (
     processing_fee decimal (5,2) not null,
     total decimal(5,2) not null
 );
+
+
+insert into sales_tax_rate (state, rate) values
+('AL', .05),
+('AK', .06),
+('AZ', .04),
+('AR', .06),
+('CA', .06),
+('CO', .04),
+('CT', .03),
+('DE', .05),
+('FL', .06),
+('GA', .07),
+('HI', .05),
+('ID', .03),
+('IL', .05),
+('IN', .05),
+('IA', .04),
+('KS', .06),
+('KY', .04),
+('LA', .05),
+('ME', .03),
+('MD', .07),
+('MA', .05),
+('MI', .06),
+('MN', .06),
+('MS', .05),
+('MO', .05),
+('MT', .03),
+('NE', .04),
+('NV', .04),
+('NH', .06),
+('NJ', .05),
+('NM', .05),
+('NY', .06),
+('NC', .05),
+('ND', .05),
+('OH', .04),
+('OK', .04),
+('OR', .07),
+('PA', .06),
+('RI', .06),
+('SC', .06),
+('SD', .06),
+('TN', .05),
+('TX', .03),
+('UT', .04),
+('VT', .07),
+('VA', .06),
+('WA', .05),
+('WV', .05),
+('WI', .03),
+('WY', .04);
+
+
+insert into processing_fee (product_type, fee) values
+('Consoles', 14.99),
+('T-Shirts', 1.98),
+('Games', 1.49);
