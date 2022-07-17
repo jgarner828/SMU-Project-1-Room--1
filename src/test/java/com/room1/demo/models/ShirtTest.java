3cc4a4bc60395057c0596db75392cefb0463c85c
Justin Garner <j.garner828@gmail.com>
2022-07-17 16:02:58 -0400

    finished model testing

--- src/test/java/com/room1/demo/models/ShirtTest.java
+++ src/test/java/com/room1/demo/models/ShirtTest.java
@@ -1,7 +1,19 @@
 package com.room1.demo.models;
 
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ShirtTest {
+
+    @Test
+    public void testThatYouCanCreateNewShirtWithArgs() {
+        Shirt newShirt = new Shirt(1, "small", "white", "shirt", BigDecimal.valueOf(10.00), 10);
+        Shirt newShirt2 = new Shirt(1, "small", "white", "shirt", BigDecimal.valueOf(10.00), 10);
+
+        assertEquals(newShirt,newShirt2);
+    }
 
 }
